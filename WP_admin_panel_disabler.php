26d85e3f81e475faf2ce01750b82f00e7cc37719
grubobeats <vladan.paunovic.bg@gmail.com>
2016-01-31 08:56:14 +0100

    Admin panel disabler

---
+++ WP_admin_panel_disabler.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+	Hook that disables top Wordpress Admin bar menu in front-end
+
+*/
+
+function hide_admin_bar_from_front_end(){
+  if (is_blog_admin()) {
+    return true;
+  }
+  return false;
+}
+add_filter( 'show_admin_bar', 'hide_admin_bar_from_front_end' );
+
+?>

7fb11dd81ed7113b7061426aebc34c89f5c8dea2
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2016-05-13 17:11:53 +0300

    Telegram API

---
+++ Telegram API.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ *
+ * Telegram Bot message sending.
+ * Syntax: https://api.telegram.org/bot<token>/METHOD_NAME
+ *
+ */
+
+function sendMessageToTelegramBot( $bot_token_number, $id_of_chat, $url_encoded_text ){
+    $bot_url = "https://api.telegram.org/bot$bot_token_number/sendMessage?chat_id=$id_of_chat&text=$url_encoded_text&parse_mode=Markdown&disable_web_page_preview=true";
+    return file_get_contents($bot_url);
+}
+
+$bot_token = "224194506:AAHN4Nt1-2dWvUmWDnQvnKC_HNTM2HgWu88"; // bot token
+$chat_id = "193777604"; // chat id, got from https://api.telegram.org/bot224194506:AAHN4Nt1-2dWvUmWDnQvnKC_HNTM2HgWu88/getUpdates
+$text = " Hello from Bot!
+    First row: *content*
+    Second row: *content*"; // message
+$encoded_text = urlencode($text); // encode message
+
+// Call to function
+sendMessageToTelegramBot( $bot_token, $chat_id, $encoded_text )
+?>
